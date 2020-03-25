4811c2646aae2e20e14fbed508b8c6b360d7095c
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-25 17:19:07 +0700

    add: style guide

---
+++ lib/styleguide.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const greetingsTitleStyle =
+    TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w500);
+
+const greetingsSubtitleStyle =
+    TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.w300);
