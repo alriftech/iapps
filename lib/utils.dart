c0060b8b8173ec53afb4fd8f73c771c6773e9a94
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-27 13:33:53 +0700

    change: main app color

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 const mainBgColor = Color(0xFFf2f2f2);
-const darkColor = Color(0xFF2A0B35);
-const midColor = Color(0xFF522349);
-const lightColor = Color(0xFFA52C4D);
+const darkColor = Color(0xFF0B3535);
+const midColor = Color(0xFF235251);
+const lightColor = Color(0xFF2CA5A3);
 const darkRedColor = Color(0xFFFA695C);
 const lightRedColor = Color(0xFFFD685A);
 
-const purpleGradient = LinearGradient(
+const blueGradient = LinearGradient(
   colors: <Color>[darkColor, midColor, lightColor],
   stops: [0.0, 0.5, 1.0],
   begin: Alignment.centerLeft,
@@ -20,3 +20,5 @@ const redGradient = LinearGradient(
     begin: Alignment.centerLeft,
     end: Alignment.centerRight
 );
+
+const USER_IMAGE = "https://avatars3.githubusercontent.com/u/13685481";
