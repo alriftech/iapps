9f6e6fdb13d038bb47526054639915a8f653acc1
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-27 19:54:54 +0700

    change: server model

--- lib/widgets/dn/server.dart
+++ lib/widgets/dn/server.dart
@@ -5,16 +5,20 @@ class Server {
   int id;
   String shortname;
   String longname;
+  String version;
   String IP;
+  DateTime updated;
 
-  Server({this.id, this.shortname, this.longname, this.IP});
+  Server({this.id, this.shortname, this.longname, this.version, this.IP, this.updated});
 
-  factory Server.createServer(object) {
+  factory Server.fromJson(json) {
     return Server(
-      id: int.parse(object['id'].toString()),
-      shortname: object['shortName'].toString(),
-      longname: object['longName'].toString(),
-      IP: object['ip'].toString(),
+      id: int.parse(json['id'].toString()),
+      shortname: json['shortName'].toString(),
+      longname: json['longName'].toString(),
+      version: json['version'].toString(),
+      IP: json['ip'].toString(),
+      updated: DateTime.parse(json['updated_at'].toString()),
     );
   }
 
@@ -24,10 +28,11 @@ class Server {
     var apiResult = await http.get(apiURL);
     var jsonObject = json.decode(apiResult.body);
 
-    List<Server> servers = [];
+    var servers = List<Server>();
     for (var server in jsonObject)
-      servers.add(Server.createServer(server));
+      servers.add(Server.fromJson(server));
 
+    print(apiResult);
     return servers;
   }
 }
