3859d7fa443a1281062dc708595ed3694028eb51
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-03-28 08:31:46 +0700

    add: splash screen & views

---
+++ lib/views/splashscreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:iapps/utils.dart';
+import 'package:iapps/views/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  _startSplash() async {
+    var duration = Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) {
+            return Home();
+          }
+        )
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startSplash();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkColor,
+      body: Center(
+        child: Image.asset("images/logo.png", width: 200.0, height: 100.0,),
+      ),
+    );
+  }
+}
